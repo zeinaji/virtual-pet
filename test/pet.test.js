0970a552e7880c04513f10a06d615e40421f41c8
Zein Aji <zeinaji97@gmail.com>
2020-03-18 20:35:32 +0000

    committed getting older

--- test/pet.test.js
+++ test/pet.test.js
@@ -5,4 +5,15 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.name).toEqual('Fido');
+    });
+    it('increases the age property by 1', () => {
+        const Lyla = new Pet('Lyla');
+        expect(Lyla.growUp()).toEqual(undefined);
+        expect(Lyla.age).toEqual(1);
+    })
 });
+
