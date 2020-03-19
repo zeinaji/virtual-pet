32af9ae745c7aea3e4bc78cc8dc787bdb51ce14a
Zein Aji <zeinaji97@gmail.com>
2020-03-19 09:45:01 +0000

    committed checkUp

--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,43 @@
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
 
 
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+}
+const max_fitness = 10;
+Pet.prototype.walk = function () {
+    if(this.fitness + 4 <= 10){
+        this.fitness += 4;
+    } else {
+        this.fitness = max_fitness;
+    }
+}
+
+const min_hunger = 0;
+Pet.prototype.feed = function() {
+    if(this.hunger -3 >= 0){
+        this.hunger -= 3;
+    } else {
+        this.hunger = min_hunger;
+    }
+}
+
+Pet.prototype.checkUp = function () {
+  if(this.fitness <= 3 && this.hunger >= 5){
+      return "I am hungry AND I need a walk";
+  } else if(this.fitness <= 3){
+        return "I need a walk";
+    } else if(this.hunger >= 5) {
+        return "I am hungry";
+    } else {
+        return "I feel great!";
+    }
 }
 module.exports = Pet;
--- test/pet.test.js
+++ test/pet.test.js
@@ -5,15 +5,63 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+
     it('sets the name property', () => {
         const pet = new Pet('Fido');
 
         expect(pet.name).toEqual('Fido');
     });
+
+describe('growUp', () => {
     it('increases the age property by 1', () => {
         const Lyla = new Pet('Lyla');
-        expect(Lyla.growUp()).toEqual(undefined);
+        Lyla.growUp();
         expect(Lyla.age).toEqual(1);
-    })
+        expect(Lyla.hunger).toEqual(5);
+        expect(Lyla.fitness).toEqual(7);
+    });
 });
 
+
+describe('walk', () => {
+    it('increases the fitness level', () => {
+        const Lyla = new Pet('Lyla');
+        Lyla.growUp();
+        Lyla.walk();
+        expect(Lyla.fitness).toEqual(10);
+        Lyla.growUp();
+        Lyla.growUp();
+        Lyla.walk();
+        expect(Lyla.fitness).toEqual(8);
+    });
+});
+
+    describe('feed', () => {
+    it('decreases hunger', () => {
+        const Lyla = new Pet('Lyla');
+        Lyla.growUp();
+        Lyla.feed();
+        expect(Lyla.hunger).toEqual(2);
+        Lyla.feed();
+        expect(Lyla.hunger).toEqual(0);
+    });
+});
+
+describe('checkUp', () => {
+    it('lets you know how the pet is feeling', () => {
+        const Lyla = new Pet('Lyla');
+        Lyla.fitness = 2;
+        expect(Lyla.checkUp()).toEqual('I need a walk');
+        Lyla.walk();
+        Lyla.hunger = 5;
+        expect(Lyla.checkUp()).toEqual('I am hungry');
+        Lyla.fitness = 2;
+        expect(Lyla.checkUp()).toEqual('I am hungry AND I need a walk');
+        Lyla.fitness = 7;
+        Lyla.hunger = 0;
+        expect(Lyla.checkUp()).toEqual('I feel great!');
+
+        
+    });
+})
+});
